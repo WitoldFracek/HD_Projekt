02a28b741553ab2a25f98219cd2dc7b6967bc32a
Witold Frącek <witek.fracek@gmail.com>
2022-06-13 21:24:23 +0200

    for loop added

--- ETL_errs/_clear_all.py
+++ ETL_errs/_clear_all.py
@@ -1,21 +1,24 @@
 from os import listdir
 from os.path import isfile, join
+import sys
 
 
-def clear_file(path):
+def clear_file(path, verbous=False):
     ext = path.split('.')[-1]
     lines = 0
-    if ext == 'py':
+    if ext != 'txt':
         return 0
     with open(path, 'r', encoding='utf-8') as file:
         lines = len(file.readlines())
     with open(path, 'w', encoding='utf-8') as file:
-        print(f'{path}: {lines} lines deleted.')
+        if verbous:
+            print(f'{path}: {lines} lines deleted.')
     return lines
 
 if __name__ == '__main__':
     files = [f for f in listdir('.') if isfile(join('.', f))]
     lines = 0
+    verbous = True if len(sys.argv) > 1 and sys.argv[1] == '-v' else False
     for f in files:
-        lines += clear_file(f)
+        lines += clear_file(f, verbous)
     print(f'Total lines deleted: {lines}')
